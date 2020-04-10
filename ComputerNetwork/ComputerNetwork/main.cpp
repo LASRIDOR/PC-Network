0bf50b415ef071b49dafb07d31bce66386331162
Nadav <nadavsuliman@gmail.com>
2020-04-10 17:17:23 +0300

    Added Network

--- ComputerNetwork/ComputerNetwork/main.cpp
+++ ComputerNetwork/ComputerNetwork/main.cpp
@@ -1,8 +1,35 @@
 #include "MainHeader.h"
+#include "List.h"
+#include "Network.h"
 #include <iostream>
+
+void initNetwork(int numOfConnections, Network &theNetwork);
+
 using namespace std;
 
 int main() {
-    cout << "Test";
-    cout << "hara mocher";
+
+    int numOfComputers;
+    int numOfConnections;
+
+    cin >> numOfComputers;
+    cin >> numOfConnections;
+
+    Network theNetwork(numOfConnections, numOfComputers);
+    initNetwork(numOfConnections, theNetwork);
+    cout << theNetwork;
+}
+
+void initNetwork(int numOfConnections, Network &theNetwork) {
+    int computerFrom, computerTo;
+
+    for (int i = 0; i < numOfConnections; ++i) {
+        cin >> computerFrom;
+        cin >> computerTo;
+
+        Computer from(computerFrom);
+        Computer to(computerTo);
+
+        theNetwork.newConnection(from, to);
+    }
 }
