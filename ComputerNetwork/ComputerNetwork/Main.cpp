0b48f2a2db8ad3b2446c8ac7f430e3bbb987ed54
Dor david Lasri <lasridor@gmail.com>
2020-04-10 12:21:28 +0300

    merge

--- ComputerNetwork/ComputerNetwork/Main.cpp
+++
@@ -1,5 +0,0 @@
-#include "MainHeader.h"
-
-int main() {
-
-}
---
+++ ComputerNetwork/ComputerNetwork/Node.h
@@ -0,0 +1,26 @@
+#ifndef __NODE_H
+#define __NODE_H
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+#pragma warning (disable: 4996)
+
+class Node {
+public:
+	Node() = default;
+	Node(Square square ,Node* next);
+	~Node();
+
+	void SetSquare(int row, int col);
+	void SetCurr_next(Node* next);
+
+	Square GetSquare();
+	Node* GetNext();
+
+private:
+	Square m_square;
+	Node* next;
+};
+
+#endif // !__NODE_H
